001c42d76683e995b5b1c159428eba57a646f735
Andreas Schneider <asn@cryptomilk.org>
2012-07-01 14:37:29 +0200

    libcamera: Add camera hal source code for msm7227.
    This is from https://github.com/Evervolv/android_hardware_qcom_camera

---
+++ libcamera/Android.mk
@@ -0,0 +1,22 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_C_FLAGS          += -O3
+LOCAL_MODULE_TAGS      := optional
+LOCAL_MODULE_PATH      := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE           := camera.$(TARGET_BOARD_PLATFORM)
+
+LOCAL_SRC_FILES        := QcomCamera.cpp
+
+LOCAL_SHARED_LIBRARIES := liblog libdl libutils libcamera_client libbinder \
+                          libcutils libhardware libui libcamera
+
+LOCAL_C_INCLUDES       := frameworks/base/services \
+                          frameworks/base/include \
+                          hardware/libhardware/include \
+                          hardware/qcom/display/libgralloc
+
+LOCAL_PRELINK_MODULE   := false
+
+include $(BUILD_SHARED_LIBRARY)
