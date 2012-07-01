d5e199d05bacc0c4a2104a83c52c19dfa4aec34f
Andreas Schneider <asn@cryptomilk.org>
2012-07-01 19:11:58 +0200

    libcamera: Add libcamera.so source code.

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -1,5 +1,44 @@
 LOCAL_PATH := $(call my-dir)
 
+####### LIBCAMERA #######
+
+ifeq ($(BOARD_USES_QCOM_CAMERA_LIBS),true)
+
+# When zero we link against libmmcamera; when 1, we dlopen libmmcamera.
+DLOPEN_LIBMMCAMERA:=1
+
+include $(CLEAR_VARS)
+
+LOCAL_PRELINK_MODULE := false
+
+LOCAL_SRC_FILES:= QualcommCameraHardware.cpp
+
+LOCAL_CFLAGS:= -DDLOPEN_LIBMMCAMERA=$(DLOPEN_LIBMMCAMERA)
+
+# Can be raised to 6 to improve framerate, at the cost of allocating
+# more ADSP memory. Use 0xa68000 as pool size in kernel to test
+LOCAL_CFLAGS += -DNUM_PREVIEW_BUFFERS=3 -D_ANDROID_
+
+LOCAL_C_INCLUDES+= \
+    $(TARGET_OUT_HEADERS)/mm-camera \
+    $(TARGET_OUT_HEADERS)/mm-still/jpeg \
+
+LOCAL_SHARED_LIBRARIES:= libutils libui libcamera_client liblog libcutils
+
+LOCAL_SHARED_LIBRARIES+= libbinder
+ifneq ($(DLOPEN_LIBMMCAMERA),1)
+LOCAL_SHARED_LIBRARIES+= liboemcamera
+else
+LOCAL_SHARED_LIBRARIES+= libdl
+endif
+
+LOCAL_MODULE:= libcamera
+include $(BUILD_SHARED_LIBRARY)
+
+endif # BOARD_USES_QCOM_CAMERA_LIBS
+
+####### CAMERA HAL #######
+
 include $(CLEAR_VARS)
 
 LOCAL_C_FLAGS          += -O3
