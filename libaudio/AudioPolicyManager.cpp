059e8c19b5f3aa55cafe4d16c94b06d89983d76f
Andreas Schneider <asn@cryptomilk.org>
2012-09-04 00:42:04 +0200

    libaudio: Add support for new device strategies.
    This fixes playing music with Apollo.

--- libaudio/AudioPolicyManager.cpp
+++ libaudio/AudioPolicyManager.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2009 The Android Open Source Project
+ * Copyright (C) 2012 Andreas Schneider <asn@cryptomilk.org>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,6 +54,18 @@ audio_devices_t AudioPolicyManager::getDeviceForStrategy(routing_strategy strate
     }
 
     switch (strategy) {
+    case STRATEGY_SONIFICATION_RESPECTFUL:
+        if (isInCall()) {
+            device = getDeviceForStrategy(STRATEGY_SONIFICATION, false);
+        } else if (isStreamActive(AudioSystem::MUSIC, SONIFICATION_RESPECTFUL_AFTER_MUSIC_DELAY)) {
+            // while media is playing (or has recently played), use the same device
+            device = getDeviceForStrategy(STRATEGY_MEDIA, false);
+        } else {
+            // when media is not playing anymore, fall back on the sonification behavior
+            device = getDeviceForStrategy(STRATEGY_SONIFICATION, false);
+        }
+
+        break;
     case STRATEGY_DTMF:
         if (!isInCall()) {
             // when off call, DTMF strategy follows the same rules as MEDIA strategy
@@ -125,11 +138,24 @@ audio_devices_t AudioPolicyManager::getDeviceForStrategy(routing_strategy strate
     break;
 
     case STRATEGY_SONIFICATION:
+        if (isInCall()) {
+            device = getDeviceForStrategy(STRATEGY_PHONE, false);
+            break;
+        }
+        // FALL THROUGH
+
+    case STRATEGY_ENFORCED_AUDIBLE:
+        // The strategy STRATEGY_ENFORCED_AUDIBLE uses same routing policy as
+        // STRATEGY_SONIFICATION except when in call where it doesn't default
+        // to STRATEGY_PHONE behavior.
         device = mAvailableOutputDevices & AudioSystem::DEVICE_OUT_SPEAKER;
         if (device == 0) {
             ALOGE("getDeviceForStrategy() speaker device not found");
         }
-        // The second device used for sonification is the same as the device used by media strategy
+
+        // The second device used for sonification is the same as the device
+        // used by media strategy.
+
         // FALL THROUGH
 
     case STRATEGY_MEDIA: {
@@ -163,7 +189,8 @@ audio_devices_t AudioPolicyManager::getDeviceForStrategy(routing_strategy strate
             device2 = mAvailableOutputDevices & AudioSystem::DEVICE_OUT_EARPIECE;
         }
 
-        // device is DEVICE_OUT_SPEAKER if we come from case STRATEGY_SONIFICATION, 0 otherwise
+        // device is DEVICE_OUT_SPEAKER if we come from case STRATEGY_SONIFICATION or
+        // STRATEGY_ENFORCED_AUDIBLE, 0 otherwise
         device |= device2;
 
         // Do not play media stream if in call and the requested device would change the hardware
